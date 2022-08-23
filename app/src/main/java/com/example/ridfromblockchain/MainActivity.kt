3f451ef66c4b8826a00ded15baa22829b2dda4d9
O.Pysmennyi <sashapysmenny@gmail.com>
2022-08-23 23:52:47 +0300

    Inital infrastructure setup settings + database connections

---
+++ app/src/main/java/com/example/ridfromblockchain/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.ridfromblockchain
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.FirebaseDatabase
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Toast.makeText(this, "Firebase connection success", Toast.LENGTH_LONG).show()
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.getReference("message")
+
+        myRef.setValue("RID from Blockchain app is successfully connected to Firebase!")
+    }
+}
