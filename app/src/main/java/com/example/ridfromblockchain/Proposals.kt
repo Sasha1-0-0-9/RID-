04e508f4498e8f3ec69663caaaf47996c2cf51d0
O.Pysmennyi <sashapysmenny@gmail.com>
2022-09-18 15:00:54 +0300

    proposals frame

---
+++ app/src/main/java/com/example/ridfromblockchain/Proposals.kt
@@ -0,0 +1,11 @@
+package com.example.ridfromblockchain
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Proposals : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_proposals)
+    }
+}
