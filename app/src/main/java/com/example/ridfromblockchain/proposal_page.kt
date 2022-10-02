a1232524de79574c844fad92425ea8299578a7a7
O.Pysmennyi <sashapysmenny@gmail.com>
2022-10-02 23:55:13 +0300

    add proposal details frame

---
+++ app/src/main/java/com/example/ridfromblockchain/proposal_page.kt
@@ -0,0 +1,11 @@
+package com.example.ridfromblockchain
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class proposal_page : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_proposal_page)
+    }
+}
